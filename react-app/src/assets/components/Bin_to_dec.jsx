00923c943c1ecf5da04ec0ae31a7318c768eb67e
Mateusz Rychel <mateusz.rychel@outlook.com>
2025-09-26 08:27:04 +0200

    Now project is working

--- react-app/src/assets/components/Bin_to_dec.jsx
+++ react-app/src/assets/components/Bin_to_dec.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
-import './App.css'
 
 function Bin_to_dec() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const algorithm = (binary) => {
+    let number = binary.split('').reverse()
+    let result = 0
+
+    for (let i = 0; i < number.length; i++) {
+      let temp = parseInt(number[i]) * (2 ** i);
+      result += temp
+    }
+    return result
+  }
+
+  const handleClick = () => {
+    const output = algorithm(input)
+    setResult(output)
+  }
 
   return (
     <>
       <div className="component">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        </div>
+        <h1>Liczba binarna na dziesiętną</h1>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Liczba binarna'></input>
+        <h1>{result}</h1>
+        <button onClick={handleClick}></button>
+      </div>
     </>
   )
 }
--- react-app/src/assets/components/Dec_to_bin.jsx
+++ react-app/src/assets/components/Dec_to_bin.jsx
@@ -1,18 +1,36 @@
 import { useState } from 'react'
-import './App.css'
 
-function Dec_to_dec() {
-  const [count, setCount] = useState(0)
+function Dec_to_bin() {
+  const [input, setInput] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const algorithm = (binary) => {
+    let x = binary
+    let temp = 0
+    let result = ""
+    while (x > 0) {
+      temp = x % 2
+      result = temp.toString() + result
+      x = Math.floor(x/2)
+    }
+    return result
+  }
+
+  const handleClick = () => {
+    const output = algorithm(input)
+    setResult(output)
+  }
 
   return (
     <>
       <div className="component">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        </div>
+        <h1>Liczba dziesiętna na binarną</h1>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Liczba dziesietna'></input>
+        <h1>{result}</h1>
+        <button onClick={handleClick}></button>
+      </div>
     </>
   )
 }
 
-export default Dec_to_dec
+export default Dec_to_bin
